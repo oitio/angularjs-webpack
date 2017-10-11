dd2834fe9f646ff306d73892c2560754e28eed46
Pascal Sancho <pascal.sancho@takoma.fr>
2017-10-11 16:08:40 +0200

    COSMETIC use my editconfig settings

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,7 @@
 module.exports = {
-  plugins: {
-    autoprefixer: {
-      browsers: ['last 2 versions']
+    plugins: {
+        autoprefixer: {
+            browsers: ['last 2 versions']
+        },
     },
-  },
 };
--- src/app/app.spec.js
+++ src/app/app.spec.js
@@ -2,19 +2,19 @@ import app from './app';
 
 describe('app', () => {
 
-  describe('AppCtrl', () => {
-    let ctrl;
+    describe('AppCtrl', () => {
+        let ctrl;
 
-    beforeEach(() => {
-      angular.mock.module(app);
+        beforeEach(() => {
+            angular.mock.module(app);
 
-      angular.mock.inject(($controller) => {
-        ctrl = $controller('AppCtrl', {});
-      });
+            angular.mock.inject(($controller) => {
+                ctrl = $controller('AppCtrl', {});
+            });
+        });
+
+        it('should contain the starter url', () => {
+            expect(ctrl.url).toBe('https://github.com/preboot/angular-webpack');
+        });
     });
-
-    it('should contain the starter url', () => {
-      expect(ctrl.url).toBe('https://github.com/preboot/angular-webpack');
-    });
-  });
 });
